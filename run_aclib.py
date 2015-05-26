990b8570a7dec10f5c9ccbf1b597f8f329389bec
Martin Goth <martin@goth-1.de>
2015-05-26 14:32:10 +0200

    Import aclib dir

--- run_aclib.py
+++ run_aclib.py
@@ -17,4 +17,16 @@ def main():
 
 
 if __name__ == '__main__':
+
+    # Workaround to import aclib modules. Maybe change this by integrating into
+    # aclib src or packaging aclib
+
+    def append_path(path):
+        import os
+        if os.path.isdir(path):
+            sys.path.insert(0, path)
+
+    append_path('../../aclib/src')
+    append_path('/home/aclib/aclib/src')
+    from downloader import Downloader
     main()
