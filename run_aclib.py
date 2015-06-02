77b23f08553f084419af71296d66f45c4eb6b477
Martin Goth <martin@goth-1.de>
2015-06-02 10:38:43 +0200

    Threadsafe installation and initialization

--- run_aclib.py
+++ run_aclib.py
@@ -4,7 +4,8 @@ import json
 import sys
 import os
 import argparse
-from threading import Thread
+from time import sleep
+import threading
 
 
 # Workaround to import aclib modules. Maybe change this by integrating into
@@ -15,6 +16,7 @@ aclib_roots = [
     "/home/aclib/aclib",
     "/vagrant/aclib"
 ]
+
 
 def import_aclib(paths):
     def import_path(path):
@@ -40,43 +42,55 @@ def main():
     config['experiment'].setdefault(
         'config_file', os.path.join(aclib_root, "src", "data", "config.json"))
     config['experiment'].setdefault('seed', 1)
-    config['experiment'].setdefault('parallel', False)
+    config['experiment'].setdefault('parallel_runs', 1)
 
     total_runs = config['experiment']['repetition']
 
     run_threads = []
+    parallel_run_block = threading.Semaphore(
+        config['experiment']['parallel_runs'])
+    install_mutex = threading.Lock()
+
     for i in range(total_runs):
-        predecessor = run_threads[-1] if (run_threads and not config['experiment']['parallel']) else None
-        run_threads.append(ExperimentRunner(i, config, predecessor))
-        run_threads[-1].start()
+        run_threads.append(ExperimentRunner(i, config, parallel_run_block, install_mutex))
+
+    spawner = threading.Thread(target=thread_spawner,
+                               args=(run_threads,
+                                     config['experiment']['parallel_runs'],
+                                     parallel_run_block),
+                               name='thread-spawner')
+    spawner.start()
 
     try:
         while True:
-            time.sleep(3600)  # Non busy waiting ;)
+            sleep(3600)  # Non busy waiting ;)
     except (KeyboardInterrupt):
             print("Exiting due to KeyboardInterrupt", file=sys.stderr)
             sys.exit(1)
 
 
-class ExperimentRunner(Thread):
+def thread_spawner(threads, max_number_threads, parallel_run_block):
+    for thread in threads:
+        parallel_run_block.acquire()
+        thread.start()
 
-    def __init__(self, counter, config, wait_for=None):
-        Thread.__init__(self, name='ac-{0:s}-{1:02d}'.format(
+
+class ExperimentRunner(threading.Thread):
+
+    def __init__(self, counter, config, end_event, install_mutex):
+        run_appedix = ('-{0:02d}'.format(counter)
+                       if config['experiment']['repetition'] != 1
+                       else '')
+
+        threading.Thread.__init__(self, name='{}{}'.format(
             config['experiment']['name'],
-            counter))
+            run_appedix))
 
-        self.previous_thread = wait_for
+        print('Initializing experiment {}'.format(self.name))
 
-        installer = install_scenario.Installer(config['experiment']['config_file'])
-        installer.install_single_scenario(config['experiment']['scenario'])
-
-        self.configurator = self.configurators[config['experiment']['configurator']](
-            config['experiment']['config_file'],
-            config['experiment']['scenario'],
-            aclib_root,
-            config['experiment']['name'])
-
-        self.configurator.prepare(config['experiment']['seed'])
+        self.install_mutex = install_mutex
+        self.release_on_exit = end_event
+        self.config = config
 
     configurators = {
         'ParamILS': runner.ParamILSRunner,
@@ -85,18 +99,32 @@ class ExperimentRunner(Thread):
     }
 
     def run(self):
-        if self.previous_thread:
-            self.previous_thread.join()
+        print('Starting experiment {}'.format(self.name))
+
+        with self.install_mutex:
+            installer = install_scenario.Installer(self.config['experiment']['config_file'])
+            installer.install_single_scenario(self.config['experiment']['scenario'])
+
+            self.configurator = self.configurators[self.config['experiment']['configurator']](
+                self.config['experiment']['config_file'],
+                self.config['experiment']['scenario'],
+                aclib_root,
+                self.name)
+
+            self.configurator.prepare(self.config['experiment']['seed'])
 
         try:
             self.configurator.run_scenario()
         except (KeyboardInterrupt, SystemExit):
             ### handle keyboard interrupt ###
             self.configurator.cleanup()
+            self.release_on_exit.release()
             return 1
+        finally:
+            self.release_on_exit.release()
+        self.release_on_exit.release()
 
-
-
+        print('Experiment {} finished'.format(self.name))
 
 
 if __name__ == '__main__':
