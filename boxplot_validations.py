25c38db46e7e2423ae6217f937fa07e82f1d96ee
Martin Goth <gothm@cs.uni-freiburg.de>
2015-10-14 16:56:44 +0200

    Join result search for validation plot scripts

--- boxplot_validations.py
+++ boxplot_validations.py
@@ -2,30 +2,25 @@
 
 '''Draw boxplot from average validation results'''
 
+from paths import validationResultPaths
 import AclibResult
 import argparse
+import logging
 import matplotlib.pyplot as plt
 import numpy as np
-import glob
-from os import listdir
-from os.path import join
+
 
 def runtimeFromMergedValidations(files):
     return np.array([np.genfromtxt(f, delimiter=',', skip_header=True, usecols=(2)) for f in files])
-
-def validationResultPaths(path):
-    resultpaths = [
-    'results/*/validate-inc/*Results*.csv',
-    'validate-test/validationResults-tunertimecsscWorkerValid_*.csv',
-    '*/SMAC/run-*/validate-inc/validationResults-traj-run-*-walltimelocal.csv']
- 
-    return [f for val in resultpaths for f in glob.glob(join(path, val))]
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('files', nargs='+')
+    parser.add_argument('--verbose', '-v', action='count', default=0)
     args = parser.parse_args()
+
+    logging.getLogger().setLevel(max(10, args.verbose * -10 + 40))
 
     scenario_data = [runtimeFromMergedValidations(validationResultPaths(d)) for d in args.files]
 
---
+++ paths.py
@@ -0,0 +1,23 @@
+import glob
+import logging
+from os.path import join
+
+def validationResultPaths(path):
+
+    resultpaths = [
+    'results/*/validate-inc/*Results*.csv',
+    'results/*/*/SMAC/run-1/validate-inc/*Results*.csv',
+    'validate-test/validationResults-tunertimecsscWorkerValid_*.csv',
+    '*/SMAC/run-*/validate-inc/validationResults-traj-run-*-walltimelocal.csv',
+    '*/SMAC/run-*/validate-inc/validationResults-tunertimelocal-run*.csv',
+    'default/SMAC/run-1/validate-def/validationResults-cli-1-walltimelocal.csv']
+
+    validation_dirs = [f for val in resultpaths for f in glob.glob(join(path, val))]
+
+    logging.debug('Found %d dirs in %s:\n%s' %
+                  (len(validation_dirs), path, '\n'.join(validation_dirs)))
+
+    if len(validation_dirs) == 0:
+        logging.error('No validation results found in %s' % path)
+
+    return validation_dirs
