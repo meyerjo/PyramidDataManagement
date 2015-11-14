e46c60192788b73901072c3fec523f444c84170b
Martin Goth <gothm@cs.uni-freiburg.de>
2015-11-14 19:01:32 +0100

    More qualitative data output

--- accloud/plots/boxplot_benchmark.py
+++ accloud/plots/boxplot_benchmark.py
@@ -3,9 +3,13 @@ import logging
 import argparse
 import csv
 import os
+import numpy as np
+import itertools
 
-def read_runresultlinematrix(f):
-    data = {}
+def read_runresultlinematrix(f, data):
+    ''' Reads a runsandresults file f and returns a dictionary with lists
+    of runtimes listed by instance
+    '''
     reader = csv.reader(f)
     header = True
     for line in reader:
@@ -13,7 +17,9 @@ def read_runresultlinematrix(f):
             header = False
             continue
         resultline = line[2].split(',')
-        data[line[0]] = [int(line[1]), resultline[0], float(resultline[1])]
+        existing_list = data.get(line[0], [])
+        existing_list.append([int(line[1]), resultline[0], float(resultline[1])])
+        data[line[0]] = existing_list
     return data
 
 def main():
@@ -24,16 +30,32 @@ def main():
 
     logging.getLogger().setLevel(max(10, args.verbose * -10 + 40))
 
+    data = {}
+
     for f in args.files:
         with open(os.path.join(f, 'validationRunResultLineMatrix-cli-1-walltimelocal.csv')) as resultfile:
-            data = read_runresultlinematrix(resultfile)
-            print f
+            data = read_runresultlinematrix(resultfile, data)
 
-            n = len(data)
-            no_crashed = [d[1] for d in data.values()].count('CRASHED')
-            print '%%CRASHED  %f' % (no_crashed / n)
+    n = len(data)
+    no_crashed = [d[1] for d in data.values()].count('CRASHED')
+    print '%%CRASHED  %f' % (no_crashed / n)
 
-            cumulated = sum((d[2] for d in data.values() if d[1] != 'CRASHED'))
-            print 'MEAN %f' % (cumulated / n)
+    cumulated = sum((d[2] for d in itertools.chain(*data.values()) if d[1] != 'CRASHED'))
+    print 'MEAN %f' % (cumulated / n)
+
+    stds = []
+    means = []   
+
+    for instance, data in data.iteritems():
+        runtimes = [d[2] for d in data]
+        if 'TIMEOUT' in (d[1] for d in data):
+            continue
+        stds.append(np.std(runtimes))
+        means.append(np.mean(runtimes))
+        # print '%s: %.2f, %.2f, %.2f' % (instance, stds[-1], means[-1], stds[-1] / means[-1])
+
+    print 'Correlation coeficient: %0.2f' % (np.corrcoef(stds, means)[0,1])
+
+
 if __name__ == '__main__':
     main()
--- accloud/plots/boxplot_runtimes.py
+++ accloud/plots/boxplot_runtimes.py
@@ -2,17 +2,20 @@
 
 '''Create boxplots of single runtimes of a target algorithm'''
 
+from __future__ import division
 from accloud.tools import ReadableDir
 from accloud import AclibResult
 import argparse
 import matplotlib.pyplot as plt
 import numpy as np
+import tabulate
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('path', action=ReadableDir)
     parser.add_argument('--timeouts', action='store_true')
+    parser.add_argument('--tablefmt', choices=tabulate.tabulate_formats, default='pipe')
     args = parser.parse_args()
 
     scenario_data = AclibResult.merge_several_runs(args.path)
@@ -26,6 +29,19 @@ def main():
         runtime_data.append((response, scenario))
 
     runtime_data = sorted(runtime_data, key=lambda d: np.median(d[0]))
+
+    def ratio_outliers(data, m = 2.):
+        d = np.abs(data - np.median(data))
+        mdev = np.median(d)
+        s = d/mdev if mdev else 0.
+        return len(data[s>m]) / len(data)
+
+    header = ['Scenario', 'Median', 'Mean', 'Std', 'Ratio Outliers']
+    table = [[name, '%.2f' % np.median(data), '%.2f' % np.mean(data), '%.2f' % np.std(data), '%.2f' % ratio_outliers(data)] for data, name in runtime_data]
+    print np.corrcoef([np.std(d) for d,_ in runtime_data],[np.mean(d) for d,_ in runtime_data])
+    print np.average(np.array([np.std(d) for d,_ in runtime_data])/np.array([np.mean(d) for d,_ in runtime_data]))
+    print(tabulate.tabulate(table, header, tablefmt=args.tablefmt))
+
     fig = plt.figure()
     boxplot = fig.add_subplot(111)
     boxplot.boxplot([time for (time, label) in runtime_data], vert=False)
