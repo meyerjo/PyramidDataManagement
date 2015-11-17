d569f2801b573aa26027971bdb6659fba15dee60
Martin Goth <gothm@cs.uni-freiburg.de>
2015-11-17 19:48:45 +0100

    Do not crash on missing files and better data output

--- accloud/plots/boxplot_benchmark.py
+++ accloud/plots/boxplot_benchmark.py
@@ -33,28 +33,32 @@ def main():
     data = {}
 
     for f in args.files:
-        with open(os.path.join(f, 'validationRunResultLineMatrix-cli-1-walltimelocal.csv')) as resultfile:
-            data = read_runresultlinematrix(resultfile, data)
+        try:
+            with open(os.path.join(f, 'validationRunResultLineMatrix-cli-1-walltimelocal.csv')) as resultfile:
+                data = read_runresultlinematrix(resultfile, data)
+        except IOError as e:
+            logging.error(e)
 
     n = len(data)
     no_crashed = [d[1] for d in data.values()].count('CRASHED')
-    print '%%CRASHED  %f' % (no_crashed / n)
-
-    cumulated = sum((d[2] for d in itertools.chain(*data.values()) if d[1] != 'CRASHED'))
-    print 'MEAN %f' % (cumulated / n)
+    print '%CRASHED  {:.0%}'.format(no_crashed / n)
 
     stds = []
     means = []   
 
+    no_timeout = 0
     for instance, data in data.iteritems():
         runtimes = [d[2] for d in data]
         if 'TIMEOUT' in (d[1] for d in data):
+            no_timeout += 1
             continue
         stds.append(np.std(runtimes))
         means.append(np.mean(runtimes))
         # print '%s: %.2f, %.2f, %.2f' % (instance, stds[-1], means[-1], stds[-1] / means[-1])
 
-    print 'Correlation coeficient: %0.2f' % (np.corrcoef(stds, means)[0,1])
+    print '%TIMEOUT  {:.0%}'.format(no_timeout / n)
+
+    print 'Correlation coeficient: %0.4f' % (np.corrcoef(stds, means)[0,1])
 
 
 if __name__ == '__main__':
