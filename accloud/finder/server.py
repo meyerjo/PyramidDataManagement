ec5446e200a3c696f17c3c5e11095719ccdec8c3
bojome <meyerjo@informatik.uni-freiburg.de>
2015-11-22 13:22:13 +0100

    * load settings on startup

--- accloud/finder/server.py
+++ accloud/finder/server.py
@@ -1,5 +1,5 @@
 '''HTTP Server and Request Handlers to explore ACcloud experiments'''
-
+import jsonpickle
 from pyramid.httpexceptions import HTTPFound
 from pyramid.config import Configurator
 from pyramid.static import static_view
@@ -14,9 +14,18 @@ def serve(**settings):
     if settings['trace']:
         config.include('pyramid_debugtoolbar')
 
+    config.registry.settings['directory_settings'] = dict()
+    config.registry.settings['reload_templates'] = True
     for root, dirs, files in os.walk(config.registry.settings['root_dir']):
         if '.settings.json' in files:
-            print(root, files, dirs)
+            root = os.path.abspath(root)
+            filename = root + '\\.settings.json'
+            with open(os.path.join(filename), "r") as myfile:
+                data = myfile.read()
+                config.registry.settings['directory_settings'][root] = \
+                    jsonpickle.decode(data)
+            config.registry.settings['directory_settings'][root]['reload'] = config.registry.settings['reload_templates']
+            config.registry.settings['directory_settings'][root]['path'] = filename
 
     dir_path = r'([\w\-\_]*\/)*'
     file_basename = r'[\w\-\_\.]*'
