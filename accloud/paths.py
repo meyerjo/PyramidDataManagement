924f8e07dd8ec91f0e62948ed36c4d84745339b8
Martin Goth <gothm@cs.uni-freiburg.de>
2015-12-03 17:43:53 +0100

    Quality objective

--- accloud/paths.py
+++ accloud/paths.py
@@ -1,10 +1,10 @@
 import glob
 import logging
-from os.path import join
+import os
 import numpy as np
 
 def find_files(resultpaths, path):
-    validation_dirs = [f for val in resultpaths for f in glob.glob(join(path, val))]
+    validation_dirs = [f for val in resultpaths for f in glob.glob(os.path.join(path, val))]
 
     logging.debug('Found %d dirs in %s:\n%s' %
                   (len(validation_dirs), path, '\n'.join(validation_dirs)))
@@ -17,11 +17,12 @@ def find_files(resultpaths, path):
 
 def benchmark_validation_files(path):
     file_paths = [
-        'results/*/validationRunResults',
-        '*/Benchmark/run-1/validationRunResultLineMatrix-cli-*-walltimelocal.csv'
+        'results/*/validationRunResultLineMatrix-cli-*-walltimelocal.csv',
+        '*/Benchmark/run-*/validationRunResultLineMatrix-cli-*-walltimelocal.csv'
     ]
 
     return find_files(file_paths, path)
+
 
 def validationResultPaths(path):
 
@@ -31,15 +32,30 @@ def validationResultPaths(path):
         'validate-test/validationResults-tunertimecsscWorkerValid_*.csv',
         '*/SMAC/run-*/validate-inc/validationResults-traj-run-*-walltimelocal.csv',
         '*/SMAC/run-*/validate-inc/validationResults-tunertimelocal-run*.csv',
-        'default/SMAC/run-1/validate-def/validationResults-cli-1-walltimelocal.csv']
+        '*/SMAC/run-1/validate-def/validationResults-cli-1-walltimelocal.csv']
+
+    return find_files(resultpaths, path)
+
+def validationQualityResultPaths(path):
+    resultpaths = [
+    'results/*/validate-inc/validationRunResultLineMatrix-traj-run-*-walltimelocal.csv',
+    '*/SMAC/run-*/validate-inc/validationRunResultLineMatrix-traj-run-*-walltimelocal.csv',
+    '*/SMAC/run-1/validate-def/validationRunResultLineMatrix-cli-1-walltimelocal.csv'
+    ]
 
     return find_files(resultpaths, path)
 
 def runtimeFromMergedValidations(files):
     return np.array([np.genfromtxt(f, delimiter=',', skip_header=True, usecols=(2)) for f in files])
 
-def sorted_scenario_data(paths):
-    sorted_data = sorted([(d, runtimeFromMergedValidations(validationResultPaths(d))) for d in paths], key=lambda d: np.median(d[1]))
+def qualityFromMergedValidations(files):
+    return np.array([np.genfromtxt(f, delimiter=',', skip_header=True, usecols=(5)) for f in files])
+
+def sorted_scenario_data(paths, objective='runtime'):
+    if objective == 'runtime':
+        sorted_data = sorted([(d, runtimeFromMergedValidations(validationResultPaths(d))) for d in paths], key=lambda d: np.mean(d[1]))
+    else:
+        sorted_data = sorted([(d, qualityFromMergedValidations(validationQualityResultPaths(d))) for d in paths], key=lambda d: np.mean(d[1]))
     names = [d[0] for d in sorted_data]
     data = [d[1] for d in sorted_data]
     return (names, data)
--- accloud/plots/boxplot_validations.py
+++ accloud/plots/boxplot_validations.py
@@ -7,31 +7,34 @@ import argparse
 import logging
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('files', nargs='+')
     parser.add_argument('--verbose', '-v', action='count', default=0)
     parser.add_argument('--output', '-o', help='Name for output files')
+    parser.add_argument('--objective', choices=['runtime', 'quality'], default='runtime')
     args = parser.parse_args()
 
     logging.getLogger().setLevel(max(10, args.verbose * -10 + 40))
 
-    names, scenario_data = paths.sorted_scenario_data(args.files)
+    names, scenario_data = paths.sorted_scenario_data(args.files, objective=args.objective)
 
-    scenario_runtimes = []
-    labels = []
-
-    fig = plt.figure()
+    fig = plt.figure(figsize=(8, 5))
+    fig.suptitle(os.path.dirname(args.files[0]).replace('_', '-'))
     boxplot = fig.add_subplot(111)
     boxplot.boxplot(scenario_data, showfliers=False)
     for i, data in enumerate(scenario_data):
         x = np.random.normal(i + 1, 0.01, size=len(data))
         boxplot.scatter(x, data, marker='.', color='r', alpha=0.4)
-    boxplot.xaxis.set_ticklabels(names)
-    boxplot.xaxis.set_label_text('scenario')
-    boxplot.set_yscale('log')
-    boxplot.yaxis.set_label_text('runtime [s]')
+    boxplot.xaxis.set_ticklabels(map(os.path.basename, names), rotation=90)
+    boxplot.xaxis.set_label_text('Machine')
+    if args.objective == 'runtime':
+        boxplot.set_yscale('log')
+        boxplot.yaxis.set_label_text('Validation Mean Runtime [s]')
+    else:
+        boxplot.yaxis.set_label_text('Validation Mean Quality')
     plt.show()
     if args.output:
         fig.tight_layout()  
