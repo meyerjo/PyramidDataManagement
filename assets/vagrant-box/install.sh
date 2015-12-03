7f175b3bf95a3588a0843f5f2c66bce7038ab47f
Martin Goth <gothm@cs.uni-freiburg.de>
2015-12-03 17:29:18 +0100

    Reduce verbosity and install numpy

--- assets/vagrant-box/install.sh
+++ assets/vagrant-box/install.sh
@@ -17,9 +17,9 @@ echo "#############################"
 # Update if last update is older than 1 day
 # Probs to http://askubuntu.com/questions/487606/bash-update-apt-get-only-if-apt-cache-is-older-than-10-minutes
 if [ "$[$(date +%s) - $(stat -c %Z /var/lib/apt/periodic/update-success-stamp)]" -ge 86400 ]; then
-    apt-get update
+    apt-get -qq update
 fi
-apt-get install -y default-jre ruby screen
+apt-get -qq install -y default-jre ruby screen python-numpy
 
 # Disable password authentication
 sed -i.bak -r -e 's/(PasswordAuthentication|ChallengeResponseAuthentication) yes/\1 no/g' /etc/ssh/sshd_config
