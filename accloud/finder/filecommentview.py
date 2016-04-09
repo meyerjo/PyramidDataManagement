a0fe1dce7213a3aad9f0cd22631c9593fae609f3
Johannes Meyer <bojome@gmail.com>
2016-04-09 14:31:54 +0200

    * refactored view processing * using request_param to split the requests up

--- accloud/finder/filecommentview.py
+++ accloud/finder/filecommentview.py
@@ -74,49 +74,83 @@ class FileCommentView():
                                                        comments=comments,
                                                        label=label,
                                                        logged_in=self.request.authenticated_userid))
-        return dict(files=[], folders=[], pagetitle='Test',
+        return dict(files=[], folders=[],
                     request=self.request, html=html, localsettingsfile=True, logged_in=self.request.authenticated_userid)
+
+    @view_config(route_name='files_comment', permission='authenticatedusers', renderer='template/index.pt',
+                     request_method='POST', request_param='comment')
+    def add_comment(self):
+        log = logging.getLogger(__name__)
+        file = self.request.matchdict['file']
+        action = self.request.matchdict['action']
+        assert (action == 'comment')
+        new_comment_entry = dict(person=self.request.authenticated_userid,
+                                 comment=self.request.POST['comment'],
+                                 time=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                                 file=file)
+
+        new_comment_str = jsonpickle.encode(new_comment_entry)
+
+        try:
+            DBSession.add(FileCommentsModel(file, new_comment_str))
+            DBSession.commit()
+        except BaseException as e:
+            log.error(str(e))
+
+        subreq = self.request.copy()
+        subreq.method = 'GET'
+        return self.request.invoke_subrequest(subreq)
+
+    @view_config(route_name='files_comment', permission='authenticatedusers', renderer='template/index.pt',
+                     request_method='POST', request_param='description')
+    def add_description(self):
+        log = logging.getLogger(__name__)
+        file = self.request.matchdict['file']
+        action = self.request.matchdict['action']
+        assert (action == 'comment')
+        new_description_entry = dict(person=self.request.authenticated_userid,
+                                     description=self.request.POST['description'],
+                                     time=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                                     file=file)
+
+        new_description_str = jsonpickle.encode(new_description_entry)
+        try:
+            DBSession.add(FileDescriptionModel(file, new_description_str, self.request.authenticated_userid))
+            DBSession.commit()
+        except BaseException as e:
+            log.error(str(e))
+
+        subreq = self.request.copy()
+        subreq.method = 'GET'
+        return self.request.invoke_subrequest(subreq)
+
+    @view_config(route_name='files_comment', permission='authenticatedusers', renderer='template/index.pt',
+                     request_method='POST', request_param='label')
+    def add_label(self):
+        log = logging.getLogger(__name__)
+        file = self.request.matchdict['file']
+        action = self.request.matchdict['action']
+        assert (action == 'comment')
+        try:
+            DBSession.add(FileLabelModel(file, self.request.POST['label']))
+            DBSession.commit()
+        except BaseException as e:
+            log.error(e.message)
+
+        subreq = self.request.copy()
+        subreq.method = 'GET'
+        return self.request.invoke_subrequest(subreq)
 
 
     @view_config(route_name='files_comment', permission='authenticatedusers', renderer='template/index.pt',
                  request_method='POST')
     def comments(self):
+        # this is the most general case, we get a post request which doesn't match any of the more general view_config
+        # thus this is the request which is executed, we write the POST array to the log and refer the request
+        # back to the normal view
         log = logging.getLogger(__name__)
-        file = self.request.matchdict['file']
-        action = self.request.matchdict['action']
-        assert (action == 'comment')
-        if 'comment' in self.request.POST:
-            new_comment_entry = dict(person=self.request.authenticated_userid,
-                                     comment=self.request.POST['comment'],
-                                     time=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-                                     file=file)
 
-            new_comment_str = jsonpickle.encode(new_comment_entry)
-
-            try:
-                DBSession.add(FileCommentsModel(file, new_comment_str))
-                DBSession.commit()
-            except BaseException as e:
-                log.error(str(e))
-        elif 'save_description' in self.request.POST:
-            new_description_entry = dict(person=self.request.authenticated_userid,
-                                         description=self.request.POST['description'],
-                                         time=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-                                         file=file)
-            new_description_str = jsonpickle.encode(new_description_entry)
-            try:
-                DBSession.add(FileDescriptionModel(file, new_description_str, self.request.authenticated_userid))
-                DBSession.commit()
-            except BaseException as e:
-                log.error(str(e))
-        elif 'save_label' in self.request.POST:
-            try:
-                DBSession.add(FileLabelModel(file, self.request.POST['label']))
-                DBSession.commit()
-            except BaseException as e:
-                log.error(e.message)
-        else:
-            logging.warning('Not yet implemented {0}'.format(self.request.POST))
+        log.warning('Not yet implemented {0}'.format(self.request.POST))
 
         subreq = self.request.copy()
         subreq.method = 'GET'
